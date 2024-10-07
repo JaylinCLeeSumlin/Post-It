21270bcce4a135e9dfe2a39aa8d0bee251743fda
JaylinCLeeSumlin <jaylin92603@gmail.com>
2024-10-07 16:25:54 -0400

    Create map to hold user info. Create mock user to test authentication

--- routes/user_router.js
+++ routes/user_router.js
@@ -2,6 +2,8 @@ var express = require("express")
 
 var router = express.Router()
 
+let users = new Map();
+
 // slash corresponds to slash that comes AFTER route stated in app.ja
 router.get("/", (req, res) => {
     
@@ -23,7 +25,14 @@ router.post("/", (req, res) => {
     let userName = req.body.username
     let password = req.body.password
 
-    if (userName == "admin" && password=="password"){
+    // create fake accounts for testing
+    userName = "admin"
+    password = "password"
+    userId = Math.floor(Math.random() * 10000) + 1
+    users.set(userName, password, userID)
+    console.log(users)
+
+    if (users.has(userName) && users.get(userName)==password){
         req.session.isValidUser = true
         req.session.userName = userName
         res.send("Welcome " + userName)
