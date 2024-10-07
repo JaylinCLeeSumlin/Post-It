f37b7e45bf897389649b6b458531cd3b55861a07
JaylinCLeeSumlin <jaylin92603@gmail.com>
2024-10-07 15:07:16 -0400

    Start on task router

---
+++ routes/task_router.js
@@ -0,0 +1,9 @@
+var express = require("express")
+
+var router = express.Router()
+
+// slash corresponds to slash that comes AFTER route stated in app.js
+router.get("/", (req, res) => {
+    
+    res.send("Enter a task")
+})
